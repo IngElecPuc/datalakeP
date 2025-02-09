a0aaaaa09ba097c9096509f20699369001f18144
Felipe Ríos <farios@uc.cl>
2025-02-09 14:15:05 -0300

    Chequeando existencia de tablas y creación

--- src/redshift_loader.py
+++ src/redshift_loader.py
@@ -1,10 +1,22 @@
 import yaml
 import boto3
+import psycopg2
 from botocore.exceptions import ClientError
+from typing import Dict, Union
 
-def get_secret(secret_name, region_name):
+def get_secret(secret_name: str, region_name: str) -> str:
     """
-    AWS base script for secret return
+    Retrieve a secret from AWS Secrets Manager.
+
+    Parameters:
+        secret_name (str): The name or ARN of the secret.
+        region_name (str): The AWS region where the secret is stored.
+
+    Returns:
+        str: The secret value as a JSON string or plain string.
+
+    Raises:
+        ClientError: If the secret retrieval fails.
     """
 
     # Create a Secrets Manager client
@@ -27,10 +39,19 @@ def get_secret(secret_name, region_name):
 
     return secret
 
-def get_config_params():
+def get_config_params() -> Dict[str, Union[str, int]]:
     """
-    Load the configuration files in a single dictionary
+    Load configuration parameters from a YAML file and AWS Secrets Manager if necessary.
+
+    Returns:
+        dict: A dictionary with Redshift connection parameters:
+            - host (str): The Redshift cluster endpoint.
+            - port (int): The port number.
+            - dbname (str): The database name.
+            - user (str): The username.
+            - password (str): The password retrieved from the config file or Secrets Manager.
     """
+
     with open('config/config.yaml', 'r') as file: 
         config = yaml.safe_load(file)
 
@@ -51,3 +72,90 @@ def get_config_params():
         'user': user,
         'password': password
     }
+
+def ensure_required_tables(config_params: Dict[str, Union[str, int]]) -> None:
+    """
+    Connect to the Redshift database, check for existing tables, and create necessary tables if missing.
+
+    Parameters:
+        config_params (dict[str, Union[str, int]]): A dictionary containing Redshift connection parameters:
+            - host: The Redshift cluster endpoint.
+            - port: The port number.
+            - dbname: The database name.
+            - user: The username.
+            - password: The password.
+
+    Returns:
+        None
+    """
+
+    try:
+        # Establish connection to Redshift
+        connection = psycopg2.connect(
+            host=config_params['host'],
+            port=config_params['port'],
+            dbname=config_params['dbname'],
+            user=config_params['user'],
+            password=config_params['password']
+        )
+        cursor = connection.cursor()
+        
+        # Query available tables, excluding internal schemes
+        query_table_names = """
+        SELECT schemaname, tablename
+        FROM pg_catalog.pg_tables
+        WHERE schemaname NOT IN ('pg_catalog', 'information_schema');
+        """
+
+        create_gender_submission = """
+        CREATE TABLE gender_submission(
+        id INTEGER IDENTITY(1,1),
+        PassengerId INTEGER NOT NULL,
+        Survived BOOLEAN NOT NULL
+        ) DISTSTYLE AUTO;
+        """
+
+        create_train_test_data = """
+        CREATE TABLE train_test_data(
+        id INTEGER IDENTITY(1,1),
+        PassengerId INTEGER NOT NULL,
+        Survived BOOLEAN NOT NULL,
+        Pclass INTEGER NOT NULL,
+        [Name] VARCHAR(100) NOT NULL,
+        Sex	VARCHAR(6) NOT NULL,
+        [Age] Decimal(4,2) NULL,
+        SibSp INTEGER NOT NULL,	
+        Parch INTEGER NOT NULL,
+        Ticket VARCHAR(20) NOT NULL,
+        Fare REAL NOT NULL,
+        Cabin VARCHAR(15) NULL,
+        Embarked CHAR(1) NULL,
+        ForTrain BOOLEAN NOT NULL
+        ) DISTSTYLE AUTO;
+        """
+
+        table_querys = {
+            'gender_submission': create_gender_submission, 
+            'train_test_data': create_train_test_data
+        }
+        
+        cursor.execute(query_table_names)
+        tables = cursor.fetchall() #Obtain all tables and schemes
+        redshift_tables  = []
+        
+        for _, tablename in tables:
+            redshift_tables.append(tablename)
+
+        for table in table_querys.keys():
+            if table not in redshift_tables :
+                cursor.execute(table_querys[table])
+                connection.commit()
+        
+    except Exception as error:
+        print("Error al conectar o consultar en Redshift:", error)
+        
+    finally:
+        if 'cursor' in locals(): #Close cursor
+            cursor.close()
+        if 'connection' in locals(): #Close connection
+            connection.close()
